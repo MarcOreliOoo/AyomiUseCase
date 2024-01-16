3b7bd65af47b285b6ecd6de1c9d22d758d1de311
MarcOreliOoo <albeaurelien@gmail.com>
2024-01-16 21:10:32 +0100

    Add RPN algo and uvicorn

---
+++ backend/RPN.py
@@ -0,0 +1,32 @@
+#It supposes that token are splitted by spaces
+def evaluate_rpn(expression):
+	calc = []
+	operators = {'+': lambda x, y: x + y,
+				 '-': lambda x, y: x - y,
+				 '*': lambda x, y: x * y,
+				 '/': lambda x, y: x / y,
+				 '^': lambda x, y: x ** y,
+				 '%': lambda x, y: x % y}
+
+	expression = expression.split()
+
+	if len(expression) % 2 == 0:
+		raise ValueError("Invalid expression")
+	
+	for token in expression:
+		if token.isdigit():
+			calc.append(int(token))
+		elif token in operators:
+			operand2 = calc.pop()
+			operand1 = calc.pop()
+			result = operators[token](operand1, operand2)
+			calc.append(result)
+		else:
+			raise ValueError("Invalid token: " + token)
+
+	return calc[0]
+
+
+expression = input("Enter an expression in Reverse Polish Notation: ")
+result = evaluate_rpn(expression)
+print("Result:", result)
