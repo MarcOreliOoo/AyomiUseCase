416286be9d758904dcc9130fceb16be2467efc4d
MarcOreliOoo <albeaurelien@gmail.com>
2024-01-17 15:54:05 +0100

    clean up

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,6 @@
 #python -m uvicorn backend.main:app --reload
 from fastapi import FastAPI
 from backend.db.database import create_db_and_tables
-""" from sqlmodel import Session, select
-from backend.db.models import Hero
-from backend.db.database import engine """
-
 
 from backend.routers import rpn
 
@@ -18,19 +14,3 @@ def on_startup():
 @app.get("/health", include_in_schema=False)
 def read_root():
     return {"Hello": "WORLD"}
-
-
-""" @app.post("/heroes/")
-def create_hero(hero: Hero):
-	with Session(engine) as session:
-		session.add(hero)
-		session.commit()
-		session.refresh(hero)
-		return hero
-
-
-@app.get("/heroes/")
-def read_heroes():
-	with Session(engine) as session:
-		heroes = session.exec(select(Hero)).all()
-		return heroes """
