44d0bf8fbd383252650bb91e1761ad2830f09704
MarcOreliOoo <albeaurelien@gmail.com>
2024-01-17 10:58:48 +0100

    Add exception handling

--- backend/RPN.py
+++ backend/RPN.py
@@ -17,6 +17,8 @@ def evaluate_rpn(expression):
 		if token.isdigit():
 			calc.append(int(token))
 		elif token in operators:
+			if len(calc) < 2:
+				raise ValueError("Invalid expression")
 			operand2 = calc.pop()
 			operand1 = calc.pop()
 			result = operators[token](operand1, operand2)
@@ -27,6 +29,7 @@ def evaluate_rpn(expression):
 	return calc[0]
 
 
-expression = input("Enter an expression in Reverse Polish Notation: ")
+""" expression = input("Enter an expression in Reverse Polish Notation: ")
 result = evaluate_rpn(expression)
 print("Result:", result)
+ """
