33a6fc10285256e38a0f108d58de3762ba5b3165
MarcOreliOoo <albeaurelien@gmail.com>
2024-01-17 10:59:06 +0100

    Add other entry points for csv + post

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,33 @@
 #python -m uvicorn main:app --reload
 
-from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.responses import FileResponse
+from RPN import evaluate_rpn
 
 app = FastAPI()
 
+class Expression(BaseModel):
+	expression: str
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-
-@app.get("/")
+@app.get("/", include_in_schema=False)
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/rpn/{expression}")
+async def calcRPN(expression: str):
+    try: 
+        return {"result": evaluate_rpn(expression)}
+    except (ValueError, TypeError) as e: 
+        return {"error": str(e)}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/rpn")
+async def calcRPN(expression: Expression):
+    return {"result": evaluate_rpn(expression.expression)}
 
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/download", response_class=FileResponse)
+async def download():
+	filePath = "path/to/data.csv"
+	response = FileResponse(filePath, media_type="text/csv")
+	response.headers["Content-Disposition"] = "attachment; filename=data.csv"
+	return response
