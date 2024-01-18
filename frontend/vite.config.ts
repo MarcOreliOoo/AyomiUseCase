f15607546ea870c7c0e12348ce2ca682f5a10271
MarcOreliOoo <albeaurelien@gmail.com>
2024-01-18 23:54:54 +0100

    update compose for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite'
-import path from "path"
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+        watch: {
+            usePolling: true,
+        },
     },
-  },
-})
+	preview:{
+		host: '0.0.0.0',
+		port: 3000,
+	},
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});
